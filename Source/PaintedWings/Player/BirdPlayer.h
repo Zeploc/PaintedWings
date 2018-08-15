2952dc2c596b255353199297064552d100c11b4e
Zeploc <alexcoultasis@gmail.com>
2018-08-15 13:44:15 +1200

    - Added base Player Character with Camera - Added Double Jump - Added Glide

--- Source/PaintedWings/Player/BirdPlayer.h
+++ Source/PaintedWings/Player/BirdPlayer.h
@@ -15,9 +15,51 @@ public:
 	// Sets default values for this character's properties
 	ABirdPlayer();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UCameraComponent* ThirdPersonCamera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class USpringArmComponent* BoomArm;
+
+
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		float BaseTurnRate;
+
+	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		float BaseLookUpRate;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	/** Called for forwards/backward input */
+	void MoveForward(float Value);
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
+
+	/**
+	* Called via input to turn at a given rate.
+	* @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	*/
+	void TurnAtRate(float Rate);
+
+	/**
+	* Called via input to turn look up/down at a given rate.
+	* @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	*/
+	void LookUpAtRate(float Rate);
+
+	float NormalGravity;
+	
+	FTimerHandle JumpHoldTimerHandle;
+	void StartGlide();
+	bool JumpHeld = false;
+
+	void StartJump();
+	void StopJump();
 
 public:	
 	// Called every frame
@@ -26,6 +68,13 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
-	
+	UPROPERTY(BlueprintReadOnly)
+		bool bIsGliding = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float GlidingGravity = 0.2f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float JumpTimeToGlide = 0.2f;
+
 };
