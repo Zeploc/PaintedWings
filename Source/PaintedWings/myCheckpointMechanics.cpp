5833db025d167ebc2a35ac59963b5ade1f4252fb
Zeploc <alexcoultasis@gmail.com>
2018-08-23 17:30:15 +1200

    - Adding Respawn back?

---
+++ Source/PaintedWings/myCheckpointMechanics.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "myCheckpointMechanics.h"
+#include "Engine/World.h"
+#include "Components/BoxComponent.h"
+#include "playerCheckpointMechanics.h"
+#include "Engine.h"
+#include "Player/BirdPlayer.h"
+#include "Player/BirdController.h"
+
+// Sets default values for this component's properties
+AmyCheckpointMechanics::AmyCheckpointMechanics()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryActorTick.bCanEverTick = true;
+	// ...
+
+	checkpointTrigger = CreateDefaultSubobject<UBoxComponent>("Box");
+	checkpointTrigger->OnComponentBeginOverlap.AddDynamic(this, &AmyCheckpointMechanics::OnOverlapBegin);
+	checkpointTrigger->OnComponentEndOverlap.AddDynamic(this, &AmyCheckpointMechanics::OnOverlapEnd);
+
+}
+
+
+// Called when the game starts
+void AmyCheckpointMechanics::BeginPlay()
+{
+	Super::BeginPlay();
+	// ...
+
+	//checkpointTrigger = GetOwner()->FindComponentByClass<UBoxComponent>();
+	//checkpointTrigger->bGenerateOverlapEvents = true;
+}
+
+
+// Called every frame
+void AmyCheckpointMechanics::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (checkpointTrigger)
+	{
+		//UE_LOG(LogTemp, Warning, TEXT("TriggerCollider;"));
+	}
+	// ...
+}
+
+void AmyCheckpointMechanics::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if (Cast<ABirdPlayer>(OtherActor))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Saving Spawn Point"));
+		Cast<ABirdController>(GetWorld()->GetFirstPlayerController())->AddNewCheckpoint(this);
+		/*if (OtherComp->GetOwner()->FindComponentByClass<UplayerCheckpointMechanics>())
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Overlap Occuring"));
+			OtherComp->GetOwner()->FindComponentByClass<UplayerCheckpointMechanics>()->m_pCurrentCheckpoint = this;
+			ABirdPlayer* PlayerRef = Cast<ABirdPlayer>(OtherActor);
+			if (PlayerRef)
+			{
+				PlayerRef->ReplenishRebase();
+			}
+		}*/
+	}
+}
+
+void AmyCheckpointMechanics::OnOverlapEnd(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+	if (Cast<ABirdPlayer>(OtherActor))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Saving Spawn Point"));
+		Cast<ABirdController>(GetWorld()->GetFirstPlayerController())->AddNewCheckpoint(this);
+		/*if (OtherComp->GetOwner()->FindComponentByClass<UplayerCheckpointMechanics>())
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Overlap Occuring"));
+			OtherComp->GetOwner()->FindComponentByClass<UplayerCheckpointMechanics>()->m_pCurrentCheckpoint = this;
+		}*/
+	}
+}
+
